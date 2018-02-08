2d73bdcf8e45aa88b823ced9f75a4d1f25e77ea1
ngavrysh <nazar@nazars-MacBook-Pro.local>
2018-02-08 11:34:24 +0200

    FIX libavro static linkage
    Packing *.so into a wheel  wasn't a good idea, as we became dependent on pyton's
    "sys.exec_prefix". This approach isn't working when installing the package in
    virtualenv. Seems like the best solution for us is to use static linkage
    for libavro.

--- build_pyavroc.sh
+++ build_pyavroc.sh
@@ -16,20 +16,72 @@
 
 set -eux
 
+STATIC=0
+[ "${1:-}" = '--static' ] && STATIC=1
+
+cd $(dirname "$0")
+
 MYDIR=$(/bin/pwd)
+
+AVRO=$MYDIR/local_avro
+
+[ -d $AVRO ] || git clone -b patches http://github.com/Tubular/avro $(basename $AVRO)
+
+# build avro
+
+if ! [ -f $AVRO/dist/lib/libavro.a ] && ! [ -f $AVRO/dist/lib/libavro.so ]
+then
+    # libavro.a must contain PIC
+    mv -n $AVRO/lang/c/src/CMakeLists.txt $AVRO/lang/c/src/orig_CMakeLists
+    cp -v $AVRO/lang/c/src/orig_CMakeLists $AVRO/lang/c/src/CMakeLists.txt
+    echo 'set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${CMAKE_SHARED_LIBRARY_C_FLAGS}")' >>$AVRO/lang/c/src/CMakeLists.txt
+
+    mkdir -p $AVRO/build $AVRO/dist
+    cd $AVRO/build
+    cmake $AVRO/lang/c -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_INSTALL_PREFIX=$AVRO/dist -DCMAKE_BUILD_TYPE=Release -DTHREADSAFE=true
+    make
+    make test
+    # workaround older cmake
+    mv $AVRO/build/avro-c.pc $AVRO/build/src/ || true
+    make install
+
+    # use static lib
+    [ $STATIC -eq 0 ] || rm -f $AVRO/dist/lib/libavro.so*
+fi
 
 PYTHON=${PYTHON:-python3.5}
 
-# avro lib to link,
-# passed as an argument, e.g `./build_pyavroc.sh libavro.so.22.0.0`
-LIBAVRO=$1
+# build avro python
+
+case $($PYTHON -c 'import sys; print(sys.version_info.major)') in
+    3) AVROPY=$AVRO/lang/py3
+        ;;
+    *) AVROPY=$AVRO/lang/py
+       ;;
+esac
+
+cd $AVROPY
+
+rm -rf build
+$PYTHON setup.py build
+
+# build pyavroc
 
 cd $MYDIR
 
 rm -rf build dist
 
-export PYAVROC_CFLAGS="-I/usr/include/avro"
-export LDFLAGS="-L/usr/lib/$LIBAVRO -Wl,-rpath,/usr/lib/$LIBAVRO"
+export PYAVROC_CFLAGS="-I$AVRO/dist/include"
+if [ $STATIC -ne 0 ]
+then
+    mkdir -p pyavroc/avro
+    cp -v local_avro/NOTICE.txt pyavroc/avro/
+    # a bit cheesy: get libraries from the cmake link.txt file
+    export PYAVROC_LIBS=$(tr ' ' '\n' <$AVRO/build/src/CMakeFiles/avro-shared.dir/link.txt | grep '^-l' | cut -c3-)
+    export LDFLAGS="-L$AVRO/dist/lib"
+else
+    export LDFLAGS="-L$AVRO/dist/lib -Wl,-rpath,$AVRO/dist/lib"
+fi
 
 $PYTHON setup.py build
 
@@ -37,11 +89,11 @@ cd build/lib*/pyavroc
 
 cd $MYDIR
 
-export PYTHONPATH=$(echo $MYDIR/build/lib*)
+export PYTHONPATH=$(echo $MYDIR/build/lib*):$(echo $AVROPY/build/lib*)
+
 cd tests
 
 $PYTHON -m pytest -sv .
 
 cd $MYDIR
-
 $PYTHON setup.py bdist_wheel
